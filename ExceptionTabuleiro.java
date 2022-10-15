b6b1d2475e5b7e4f1a694b290591f6da48768d31
Kaliel Kautnick <95942810+Kalielkautnick@users.noreply.github.com>
2022-10-14 21:16:49 -0300

    Add files via upload

---
+++ ExceptionTabuleiro.java
@@ -0,0 +1,9 @@
+package tabuleiro;
+
+public class ExceptionTabuleiro extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionTabuleiro(String msg) {
+		super(msg);
+	}
+}
