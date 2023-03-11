274838fd8901987660dc399e8d31e190f34b9575
Kaliel Kautnick <95942810+Kalielkautnick@users.noreply.github.com>
2023-03-11 15:08:42 -0300

    Delete Tabuleiro directory

--- Tabuleiro/ExceptionTabuleiro.java
+++
@@ -1,9 +0,0 @@
-package tabuleiro;
-
-public class ExceptionTabuleiro extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public ExceptionTabuleiro(String msg) {
-		super(msg);
-	}
-}
